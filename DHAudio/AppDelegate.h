fcb138ce6a2e581cad865438ba9214948e1e2647
鲸涛 <hongsen.hhs@alibaba-inc.com>
2017-06-06 14:20:26 +0800

    Initial Commit

---
+++ DHAudio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DHAudio
+//
+//  Created by Huang Hongsen on 17/6/6.
+//  Copyright © 2017年 Huang Hongsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DHAudio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DHAudio
+//
+//  Created by Huang Hongsen on 17/6/6.
+//  Copyright © 2017年 Huang Hongsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
